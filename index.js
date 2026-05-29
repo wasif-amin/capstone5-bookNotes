c0ece5dc5ebe6ddc898bb7c404606f9e5286da86
wasif-amin <aminwasif96@gmail.com>
2026-05-29 17:17:29 -0400

    added edit route for UPDATE

--- index.js
+++ index.js
@@ -47,16 +47,54 @@ app.post("/new", async (req, res) => {
   const rating = req.body.rating;
   const isbn = req.body.isbn;
   try {
-    await db.query("INSERT INTO books (title, description, rating, isbn) VALUES ($1, $2, $3, $4)",[title, description, rating, isbn]);
+    await db.query(
+      "INSERT INTO books (title, description, rating, isbn) VALUES ($1, $2, $3, $4)",
+      [title, description, rating, isbn]
+    );
     res.redirect("/");
   } catch (error) {
     console.log(error);
     res.redirect("/");
-
-  
   }
 });
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId;
+  try {
+    await db.query("DELETE FROM books WHERE id = $1", [id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.get("/edit/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await db.query("SELECT * FROM books WHERE id = $1", [id]);
+  const entry = result.rows[0];
+  res.render("edit.ejs", { book: entry });
+});
+app.post("/edit", async (req, res) => {
+  // 🌟 ADD THESE THREE LINES FOR DEBUGGING:
+  console.log("===============================");
+  console.log("DATA RECEIVED FROM FORM:", req.body);
+  console.log("===============================");
 
+  const id = req.body.id;
+  const title = req.body.updatedTitle;
+  const rating = req.body.updatedRating;
+  const isbn = req.body.updated_isbn;
+  const description = req.body.description;
+
+  try {
+    await db.query(
+      "UPDATE books SET title = $1, rating = $2, description = $3, isbn = $4 WHERE id = $5",
+      [title, rating, description, isbn, id]
+    );
+    res.redirect("/");
+  } catch (err) {
+    console.log("Error updating database:", err);
+    res.status(500).send("Database update failed.");
+  }
+});
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
